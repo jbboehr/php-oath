24e07c5bbd96816c8f5a3aae40196ff17bdd9b5e
John Boehr <john@johnboehr.is>
2018-11-11 01:10:12 -0800

    Add Nix and PHP 7.3 to the build matrix

--- default.nix
+++ default.nix
@@ -1,22 +1,13 @@
-# when using nix-shell, run "configurePhase" otherwise you'll have missing headers
-# to use a specific version of php, run `nix-shell --arg php '(import <nixpkgs> {}).php56'`
+{
+  pkgs ? import <nixpkgs> {},
+  php ? pkgs.php,
 
-{ php ? (import <nixpkgs> {}).php, pkgs ? import <nixpkgs> {} }:
+  phpOathVersion ? null,
+  phpOathSrc ? ./.,
+  phpOathSha256 ? null
+}:
 
-let
-  buildPecl = import <nixpkgs/pkgs/build-support/build-pecl.nix> {
-    inherit php;
-    inherit (pkgs) stdenv autoreconfHook fetchurl;
-  };
-in
-
-buildPecl rec {
-  name = "oath-${version}";
-  version = "1.0.0";
-  src = ./.;
-  doCheck = true;
-  buildInputs = [ pkgs.oathToolkit pkgs.pkgconfig ];
-  checkTarget = "test";
-  checkFlagsArray = ["REPORT_EXIT_STATUS=1" "NO_INTERACTION=1" "TEST_PHP_DETAILED=1"];
+pkgs.callPackage ./derivation.nix {
+  inherit php phpOathVersion phpOathSrc phpOathSha256;
 }
 
---
+++ derivation.nix
@@ -0,0 +1,27 @@
+{ php, stdenv, autoreconfHook, fetchurl, oathToolkit, pkgconfig,
+  phpOathVersion ? null, phpOathSrc ? null, phpOathSha256 ? null }:
+
+let
+  orDefault = x: y: (if (!isNull x) then x else y);
+  buildPecl = import <nixpkgs/pkgs/build-support/build-pecl.nix> {
+    inherit php stdenv autoreconfHook fetchurl;
+  };
+in
+
+buildPecl rec {
+  name = "oath-${version}";
+  version = orDefault phpOathVersion "5bf7f88ab2644ae34e1983d698f2645d4d78308d";
+  src = orDefault phpOathSrc (fetchurl {
+    url = "https://github.com/jbboehr/php-oath/archive/${version}.tar.gz";
+    sha256 = orDefault phpOathSha256 "0misil8s72vs5l0w6xkkfkc87spvwwrr7dnx4acci45gdx865q8m";
+  });
+
+  makeFlags = ["phpincludedir=$(out)/include/php/ext/oath"];
+  buildInputs = [ oathToolkit ];
+  nativeBuildInputs = [ pkgconfig ];
+
+  doCheck = true;
+  checkTarget = "test";
+  checkFlagsArray = ["REPORT_EXIT_STATUS=1" "NO_INTERACTION=1" "TEST_PHP_DETAILED=1"];
+}
+
